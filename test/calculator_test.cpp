a5b76ee3aeb9624c46e54cfde8bf400caaeb679c
Kelvin Robles <kelvin.robles018@gmail.com>
2017-05-03 13:53:30 +0200

    -Cleaned CMakeLists.txt and added comments -Added dummy TEST in test/calculator_test.cpp

--- test/calculator_test.cpp
+++ test/calculator_test.cpp
@@ -4,6 +4,11 @@
 #include "gtest/gtest.h"
 //#include "gmock/gmock.h"
 
+TEST(DummyTest, FirstTest)
+{
+	EXPECT_EQ(1, 1);
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
