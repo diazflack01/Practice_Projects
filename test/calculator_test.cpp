3deaef3baa9462aef4ca920bca5f6802e0d11b15
Kelvin Robles <kelvin.robles018@gmail.com>
2017-05-03 11:59:53 +0200

    created test dir with empty test file added and modified CMakeLists.txt to make googletest work

---
+++ test/calculator_test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <Calculator.hpp>
+#include "gtest/gtest.h"
+//#include "gmock/gmock.h"
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
